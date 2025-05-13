de21e0aebb94e53e274cc63583687ee2dcfd7dbd
Loy Yee Ko <koloyyee@gmail.com>
2025-05-12 23:17:36 -0400

    w1 clojure csv to seq Milk

--- src/main.clj
+++ src/main.clj
@@ -1,13 +1,26 @@
-(ns main)
+(ns main
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
 (defn -main []
   (println "Hello World!"))
 
-(defrecord Person [name age male?])
-(def d (Person. "david" 39 true))
-(-> d :age)
-(:name d)
-(def g (assoc d :name "Geroge"))
+(println (seq (.list (java.io.File. "resources"))))
+(print (slurp  (io/resource "nms_strontium90_milk_ssn_strontium90_lait.csv")))
+(def csv (str/replace (slurp  "resources/nms_strontium90_milk_ssn_strontium90_lait.csv") "\r" ""))
+(print csv)
 
-d
-g
+(defrecord Milk [sample type start-date stop-date station-name province sr90-activity sr90-error sr90-calcium])
+
+(defn process-data []
+  (let [lines (rest (str/split csv #"\n"))
+        milk-data (map
+                   (fn [line]
+                     (let [[sample type start-date stop-date station provence sr90-activity sr90-error sr90-calcium] (str/split line #",")]
+                       (Milk. sample type start-date stop-date station provence sr90-activity sr90-error sr90-calcium)))
+                   lines)]
+    milk-data))
+
+(process-data)
+((complement nil?))
